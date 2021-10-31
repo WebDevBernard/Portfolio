ca99fd44d7ba4c8fbc20d42df9701af3b5bacde0
Bernard Yang <ybernard@gmail.com>
2021-10-31 02:47:08 -0700

    reworked refs

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -8,26 +8,38 @@ export default function Form({
 }) {
   return (
     <form
-      className={classes.form}
       name="contact"
-      method="POST"
+      className={classes.form}
+      onSubmit={submitHandler}
       data-netlify="true"
+      method="POST"
     >
       <input type="hidden" name="form" value="contact" />
       <div className={classes.control}>
-        <input id="form" placeholder="Name" type="text" name="name" required />
+        <input
+          id="form"
+          placeholder="Name"
+          name="name"
+          ref={nameInputRef}
+          required
+        />
       </div>
       <div className={classes.control}>
-        <input type="email" placeholder="Email" name="email" required />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          ref={emailInputRef}
+          required
+        />
       </div>
       <div className={classes.control}>
         <textarea
           placeholder="Message"
           rows="5"
-          name="message"
+          ref={descriptionInputRef}
           required
         ></textarea>
-
         <div className={classes.actions}>
           <button>submit</button>
         </div>
