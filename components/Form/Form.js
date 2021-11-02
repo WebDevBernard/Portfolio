4908b4b5f60c069d82e8bf74f92649775ab91259
Bernard Yang <ybernard@gmail.com>
2021-11-02 05:55:52 -0700

    add more css styling

--- components/Form/Form.js
+++ components/Form/Form.js
@@ -1,26 +1,6 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import classes from "./Form.module.css";
 
-const schema = yup.object().shape({
-  name: yup.string().required("Full name field is required"),
-  email: yup
-    .string()
-    .email("Invalid email")
-    .required("Email field is required"),
-  message: yup.string().required("Message field is required"),
-});
-
 export default function Form() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
   return (
     <form
       name="contact"
@@ -31,35 +11,14 @@ export default function Form() {
       <input type="hidden" name="form" value="contact" />
 
       <div className={classes.control}>
-        <input
-          id="form"
-          placeholder="Name*"
-          name="name"
-          {...register("name", { required: true })}
-          required
-        />
+        <input id="form" placeholder="Name*" name="name" />
+      </div>
+      <div className={classes.control}>
+        <input type="email" name="email" placeholder="Email*" />
+      </div>
+      <div className={classes.control}>
+        <textarea name="message" placeholder="Message*" rows="5"></textarea>
 
-        <p className={classes.errors}>{errors.name?.message}</p>
-      </div>
-      <div className={classes.control}>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email*"
-          {...register("email", { required: true })}
-          required
-        />
-        <p className={classes.errors}>{errors.email?.message}</p>
-      </div>
-      <div className={classes.control}>
-        <textarea
-          name="message"
-          placeholder="Message*"
-          rows="5"
-          {...register("message", { required: true })}
-          required
-        ></textarea>
-        <p className={classes.errors}>{errors.message?.message}</p>
         <div className={classes.actions}>
           <button>Submit</button>
         </div>
