cf87ea7cd4cabfdf087b852f93d39ddf4c9e0340
Bernard Yang <ybernard@gmail.com>
2021-11-02 22:02:14 -0700

    fixed font

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ export default function Home() {
         <title>Bernard Yang</title>
         <meta name="Bernard Yang" content="Portfolio" />
         <link
+          href="https://fonts.googleapis.com/css2?family=Assistant&family=Inter:wght@200&display=swap"
+          rel="stylesheet"
+        />
+        <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.14.0/devicon.min.css"
         ></link>
