c714c6bdc8f91efc8a1ebc0ba397b31f29a9d8e0
Bernard Yang <ybernard@gmail.com>
2021-11-21 02:42:31 -0800

    update description

--- components/About/About.js
+++ components/About/About.js
@@ -13,10 +13,10 @@ export default function About({ aboutRef }) {
             I am a front end developer who recently completed the Lighthouse
             Labs Web Development Program. What I enjoy the most about coding is
             making my personal projects and learning how to automate things with
-            code. I am eager to learn any languages and skills that will help me
-            become a better programmer. When I am not coding, I enjoying
-            watching movies and playing videogames. If you would like to chat,
-            leave a message below and I will respond shortly.
+            code. I am really interested in developing in the DeFi space but am
+            open to learning anything that will help me become become a better
+            programmer. When I am not coding, I enjoying watching movies and
+            playing videogames.
           </p>
         </div>
       </span>
