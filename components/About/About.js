5539ce6be20f4af1be2517c9d9918860f50d9dc9
Bernard Yang <ybernard@gmail.com>
2022-02-05 00:32:48 -0800

    update about

--- components/About/About.js
+++ components/About/About.js
@@ -10,10 +10,11 @@ export default function About({ aboutRef }) {
         <p className={classes.project_title}>About me</p>
         <div>
           <p className={classes.aboutcard}>
-            I am a front end developer who enjoys making apps that help automate
-            simple tasks. Alongside web development, I am interested in learning
-            about developing in the DeFi space. When I am not coding, I enjoy
-            watching movies or playing a bit of WoW.
+            I am a front end developer who enjoys finding ways to automate
+            things with code. I love working on my personal projects and I
+            always spend extra time on the design and try to make them look
+            good. When I am not coding, I enjoy watching movies or playing a bit
+            of WoW.
           </p>
         </div>
       </span>
