32215897862188c489c558af12bc98643afb4963
Bernard Yang <ybernard@gmail.com>
2022-02-24 07:48:22 -0800

    test forms

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -31,31 +31,33 @@ export default function Contact() {
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setState({
       ...state,
-      [e.currentTarget.id]: e.currentTarget.value,
+      [e.currentTarget.name]: e.currentTarget.value,
     });
   };
 
   return (
     <div className="space-y-6 mx-auto flex flex-col  md:w-4/5">
-      <span className="col-span-2 flex self-start items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-indigo-900"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-          />
-        </svg>
-        <h2 className="self-start text-lg font-bold text-indigo-900">
-          Contact Me
-        </h2>
-      </span>
+      {!submitted && (
+        <span className="col-span-2 flex self-start items-center space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-indigo-900"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg>
+          <h2 className="self-start text-lg font-bold text-indigo-900">
+            Contact Me
+          </h2>
+        </span>
+      )}
       {!submitted && (
         <form
           className="mx-auto w-full flex flex-col space-y-4 md:items-center"
