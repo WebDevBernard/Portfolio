12fdc391f88ff01c977e3a4331f2edea3bce6db4
Bernard Yang <ybernard@gmail.com>
2022-02-25 14:39:40 -0800

    update project description

--- data/project-data.ts
+++ data/project-data.ts
@@ -24,7 +24,7 @@ export const projects: Projects[] = [
     image:
       "https://raw.githubusercontent.com/WebDevBernard/Portfolio/main/docs/twitter.png",
     description:
-      "A simple Twitter-like React app with Firebase authentication.  Users can tweet, like, comment on other tweets.",
+      "A simple Twitter-like React app with Firebase authentication.  Users can tweet, like, and comment on other tweets.",
     github: "https://github.com/WebDevBernard/Twitter-React",
     link: "https://twitterreact.vercel.app/",
   },
