12696c815914d7292d6560e4554fdf904e9d5cfe
Bernard Yang <ybernard@gmail.com>
2022-02-24 20:43:28 -0800

    fixed responsive design on hero element

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,12 +3,12 @@ const Hero: FC<{ scrollContact: () => void }> = ({ scrollContact }) => {
   return (
     <div className="flex justify-evenly items-center flex-col md:flex-row">
       {/* left */}
-      <div className="flex flex-col justify-center items-center md:items-start md:w-1/2">
-        <h1 className="ml-1 md:ml-0  text-2xl leading-tight tracking-wider">
+      <div className="w-full flex flex-col justify-center items-center md:items-start md:w-1/2">
+        <h1 className="ml-8 md:ml-0  text-2xl leading-tight tracking-wider">
           <span className="font-bold text-4xl text-indigo-900 mb-24">
             Hello!
           </span>
-          <span className="whitespace-nowrap">
+          <span>
             <br />
             I am Bernard,
             <br />a <span className="highlight ">software developer</span> in
