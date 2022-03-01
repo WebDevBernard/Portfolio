6f18190252ab3bb391edca7e027e146eee71549e
Bernard Yang <ybernard@gmail.com>
2022-03-01 13:51:09 -0800

    update descript

--- data/project-data.ts
+++ data/project-data.ts
@@ -22,7 +22,7 @@ export const projects = [
     title: "Make My Day",
     image: "/docs/kanban.png",
     description:
-      "A project management app and Kanban board for teams to manage projects and organize tasks.  Full-stack app: React frontend, Express backend and Postgres database",
+      "A project management app and Kanban board for teams to manage projects and organize tasks.  Full-stack app: React frontend, ExpressJS backend and PostgreSQL database.",
     github: "https://github.com/WebDevBernard/MakeMyDay",
     link: "https://makemydaydemo.vercel.app/",
   },
