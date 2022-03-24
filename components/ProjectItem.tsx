6efd55bdd23c453ee577ce4e8aca76be2caee872
Bernard Yang <ybernard@gmail.com>
2022-03-24 01:05:33 -0700

    remove project item padding

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -19,7 +19,7 @@ const ProjectItem: FC<ProjectProps> = (props) => {
   return (
     <div
       className="flex flex-col justify-between 
-    p-4 overflow-hidden  relative"
+ overflow-hidden  relative"
     >
       {openModal && (
         <Description
